fd57025b311767b43138cd8410805513b0c75eb0
I Gede Deindra Dwija Puridiasta <114994571+DeindraDwija@users.noreply.github.com>
2023-07-06 15:41:20 +0800

    Add files via upload

---
+++ latweb1/web/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Desain Web dengan Foto Profil</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+
+        .profile {
+            background-color: #fff;
+            border-radius: 5px;
+            padding: 20px;
+            margin-top: 30px;
+            text-align: center;
+        }
+
+        .profile h2 {
+            color: #333;
+            margin-bottom: 10px;
+        }
+
+        .profile p {
+            margin: 0;
+        }
+
+        .profile img {
+            width: 200px;
+            height: 200px;
+            border-radius: 50%;
+            object-fit: cover;
+            margin-bottom: 20px;
+        }
+
+        .profile label {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Profil Saya</h1>
+        <div class="profile">
+            <img src="https://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/810595323-1629796136.jpg" alt="Foto Profil">
+            <h2>Ilmu Komputer</h2>
+            <p><label>Nama:</label> I Gede Deindra Dwija Puridiasta</p>
+            <p><label>Kelas:</label> 4 IKI</p>
+            <p><label>NIM:</label> 2115101021</p>
+        </div>
+    </div>
+</body>
+</html>
